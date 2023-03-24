a673df088aac1f0f9a6b7fbe70482e08741e8d2c
diegodestf <diegodesteffano@gmail.com>
2023-03-24 14:58:15 -0300

    Initial commit

---
+++ routes/openaiRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+const { generateImage } = require('../controllers/openaiController');
+
+router.post('/generateImage', generateImage);
+
+module.exports = router;
